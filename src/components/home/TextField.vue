c4f47b7a62daca556b31c920d8dd6073591a64b4
Hiroaki Date <mmst3l4@gmail.com>
2022-03-05 16:34:38 +0900

    TextFieldコンポーネントを実装

---
+++ src/components/home/TextField.vue
@@ -0,0 +1,33 @@
+<template>
+  <input type="text" :value="modelValue" @input="handleChangeText" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TextField',
+
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+
+  emits: {
+    'update:modelValue': (updatedModelValue: string) => true,
+  },
+
+  setup(_, { emit }) {
+    const handleChangeText = (event: Event) => {
+      const target = event.target as HTMLInputElement;
+      emit('update:modelValue', target.value);
+    };
+
+    return {
+      handleChangeText,
+    };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,34 @@
 <template>
   <div class="home">
-    <Editor v-model="state.text" @submit="submit" />
+    <div>
+      text: {{ state.text }}
+      <Editor v-model="state.text" @submit="submit" />
+    </div>
+
+    <div>
+      checked: {{ state.name }} <br />
+      <TextField v-model="state.name" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive } from 'vue';
 import Editor from '@/components/home/Editor.vue';
+import TextField from '@/components/home/TextField.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     Editor,
+    TextField,
   },
 
   setup() {
-    const state = reactive<{ text: string }>({
+    const state = reactive({
       text: 'hogefuga',
+      names: ['hiroaki'],
+      name: 'hiroaki',
     });
 
     const submit = (text: string) => {
