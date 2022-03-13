bb1531782f3d11294d2508a58d62b1b077a467ae
Hiroaki Date <mmst3l4@gmail.com>
2022-03-13 11:17:48 +0900

    [from now] 2022/03/13 11:17:48
    diff --git a/src/views/Home.vue b/src/views/Home.vue
    index 89fc1cc..504859b 100644
    --- a/src/views/Home.vue
    +++ b/src/views/Home.vue
    @@ -1,52 +1,34 @@
     <template>
       <div class="home">
    -    <!-- <div>
    -      text: {{ state.text }}
    -      <Editor v-model="state.text" @submit="submit" />
    -    </div> -->
    -
    -    <div>
    -      name: {{ state.name }} <br />
    -      <Child v-model="state.name" />
    -    </div>
    -
    -    <div>
    -      names: {{ state.names }} <br />
    -      <Checkbox v-model="state.names" value-text="hiroaki" />
    -      <Checkbox v-model="state.names" value-text="yukari" />
    -      <Checkbox v-model="state.names" value-text="nanaka" />
    -    </div>
    +    <button @click="buttonClick" :disabled="state.disabled">click</button>
       </div>
     </template>
    
     <script lang="ts">
     import { defineComponent, reactive } from 'vue';
    -import Editor from '@/components/home/Editor.vue';
    -import Child from '@/components/home/Child.vue';
    -import Checkbox from '@/components/home/Checkbox.vue';
    
     export default defineComponent({
       name: 'Home',
    -  components: {
    -    // Editor,
    -    Child,
    -    Checkbox,
    -  },
    
       setup() {
         const state = reactive({
    -      text: 'hogefuga',
    -      names: ['hiroaki'],
    -      name: 'hiroaki',
    +      disabled: false,
         });
    
    -    const submit = (text: string) => {
    -      console.log(`submit: ${text}`);
    +    const buttonClick = () => {
    +      state.disabled = true;
    +
    +      if (state.disabled) {
    +        setTimeout(() => {
    +          console.log('hello');
    +          state.disabled = false;
    +        }, 500);
    +      }
         };
    
         return {
    +      buttonClick,
           state,
    -      submit,
         };
       },
     });

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,34 @@
 <template>
   <div class="home">
-    <!-- <div>
-      text: {{ state.text }}
-      <Editor v-model="state.text" @submit="submit" />
-    </div> -->
-
-    <div>
-      name: {{ state.name }} <br />
-      <Child v-model="state.name" />
-    </div>
-
-    <div>
-      names: {{ state.names }} <br />
-      <Checkbox v-model="state.names" value-text="hiroaki" />
-      <Checkbox v-model="state.names" value-text="yukari" />
-      <Checkbox v-model="state.names" value-text="nanaka" />
-    </div>
+    <button @click="buttonClick" :disabled="state.disabled">click</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive } from 'vue';
-import Editor from '@/components/home/Editor.vue';
-import Child from '@/components/home/Child.vue';
-import Checkbox from '@/components/home/Checkbox.vue';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    // Editor,
-    Child,
-    Checkbox,
-  },
 
   setup() {
     const state = reactive({
-      text: 'hogefuga',
-      names: ['hiroaki'],
-      name: 'hiroaki',
+      disabled: false,
     });
 
-    const submit = (text: string) => {
-      console.log(`submit: ${text}`);
+    const buttonClick = () => {
+      state.disabled = true;
+
+      if (state.disabled) {
+        setTimeout(() => {
+          console.log('hello');
+          state.disabled = false;
+        }, 500);
+      }
     };
 
     return {
+      buttonClick,
       state,
-      submit,
     };
   },
 });
